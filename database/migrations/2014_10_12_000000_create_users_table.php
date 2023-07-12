78e953c772e0ac406ed143e39fc845c42706704b
Kenneth Kipchumba <branrono@gmail.com>
2023-07-12 05:47:09 +0300

    1. Added respondents' column fields onto the users table since respondents are eventually going to be system users.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,17 +18,52 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('ext_no')->nullable();
             $table->string('email')->unique();
-            $table->string('national_id');
+            $table->string('national_id')->unique();
             $table->string('phone_1');
             $table->string('phone_2')->nullable();
             $table->string('phone_3')->nullable();
+            $table->string('phone_4')->nullable();
             $table->string('gender');
             $table->date('dob')->nullable();
+            $table->string('national_id')->nullable();
+            $table->string('occupation')->nullable();
+            $table->string('region')->nullable();
+            $table->string('county')->nullable();
+            $table->string('sub_county')->nullable();
+            $table->string('constituency')->nullable();
+            $table->string('ward')->nullable();
+            $table->string('sampling_point')->nullable();
+            $table->string('setting')->nullable();
+            // $table->string('district')->nullable();
+            // $table->string('division')->nullable();
+            // $table->string('location')->nullable();
+            // $table->string('sub_location')->nullable();
+            $table->string('exact_age')->nullable();
+            $table->string('education_level')->nullable();
+            $table->string('marital_status')->nullable();
+            $table->string('religion')->nullable();
+            $table->string('income')->nullable();
+            $table->string('Lsm')->nullable();
+            $table->string('ethnic_group')->nullable();
+            $table->string('employment_status')->nullable();
+            $table->string('age_group')->nullable();
+            $table->dateTime('interview_date_time')->nullable();
+            $table->dateTime('interview_status')->nullable();
+            $table->string('last_downloaded_date')->nullable();
+
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
+
+            
+            
+            
+            
+            
+            
+            
         });
     }
 
