ae814c3ce594f4b0db7522d36752072bda3eed38
Kenneth Kipchumba <branrono@gmail.com>
2024-04-04 11:33:46 +0300

    1. Survey Results JSON Export     - renamed `id` field to `interview_id` for better clarity.

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -405,10 +405,14 @@ class ResultController extends Controller
         $results = Result::query()
             ->join('interviews', 'results.interview_id', '=', 'interviews.id')
             ->join('users', 'results.user_id', '=', 'users.id')
-            ->select('interviews.id','results.content')
+            ->select('interviews.id as interview_id','results.content')
             ->where('results.schema_id', $schemaId)
             ->where('interviews.interview_status', 'Interview Completed')
-            ->where('interviews.quality_control', '<>', 'Cancelled')
+            ->where(function ($query)
+            {
+                $query->where('interviews.quality_control', '<>', 'Cancelled')
+                      ->orWhereNull('interviews.quality_control');
+            })
             ->get();
 
         //dd($results);
