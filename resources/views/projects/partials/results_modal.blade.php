3e789c813bf3fc18a1006bbba87df90c0381b888
Kenneth Kipchumba <branrono@gmail.com>
2024-06-06 10:31:49 +0300

    1. Survey Results Download.     - Removed redundunt PDF, CSV and XLSX Download formats as they were not working properly.     - Enabled JSON Results download to include results from deleted respondents.

--- resources/views/projects/partials/results_modal.blade.php
+++ resources/views/projects/partials/results_modal.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="resultsLabel">
-          Download {{ $survey->survey_name }} Results
+          Download {{ $survey->survey_name }}
         </h5>
         <button type="button" class="btn-close" data-coreui-dismiss="modal" aria-label="Close"></button>
       </div>
@@ -21,32 +21,18 @@
               <tr>
                 <td>
                   <div class="btn-group btn-group-sm" role="group" aria-label="Scripter Actions">
-                    <a href="{{ url('csv_export', $survey->id) }}" class="btn btn-outline-warning btn-sm" title="CSV Format">
-                      <i class="fas fa-file-csv"></i>
-                      CSV
-                    </a>
-                    <a href="{{ url('xlsx_sheets_export', $survey->id) }}" class="btn btn-outline-primary btn-sm" title="Excel Sheets">
-                      Sheets
-                    </a>
-                    <a href="{{ url('xlsx_export', $survey->id) }}" class="btn btn-outline-dark btn-sm" title="Excel Format">
-                      <i class="far fa-file-spreadsheet" style="color: #3d3846;"></i>
-                      Survey Results XLSX
-                    </a>
+                    
                     <a href="{{ url('interviews_xlsx_export', $survey->id) }}" class="btn btn-outline-dark btn-sm" title="Excel Format">
-                      <i class="fa-solid fa-file-xls" style="color: #3d3846;"></i>
+                      <i class="fa-solid fa-file-excel" style="color: #3d3846;"></i>
                       Interviews XLSX
                     </a>
-                    <a href="{{ url('pdf_export', $survey->id) }}" class="btn btn-outline-dark btn-sm" title="Portable Document Format">
-                      <i class="fas fa-file-pdf" style="color: #ef2929;"></i>
-                      PDF
-                    </a>
                     <a href="{{ route('json_export', $survey->id) }}" class="btn btn-outline-primary btn-sm" title="JSON Format">
-                      <i class="fas fa-brackets-curly"></i>
-                      JSON
+                      <i class="fa-solid fa-file-export"></i>
+                      Survey Results JSON
                     </a>
                     <a href="{{ route('xml_export', $survey->id) }}" class="btn btn-outline-info btn-sm" title="XML Format">
-                      <i class="fas fa-brackets-curly"></i>
-                      XML
+                      <i class="fa-solid fa-file-arrow-down"></i>
+                      Survey Results XML
                     </a>
                   </div>
                 </td>
